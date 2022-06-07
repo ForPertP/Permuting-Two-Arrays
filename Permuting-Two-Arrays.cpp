345dbcf49c5b7e22b678b7884b4d0f23058635e8
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-06-08 02:05:02 +0900

    Update Permuting-Two-Arrays.cpp

--- Permuting-Two-Arrays.cpp
+++ Permuting-Two-Arrays.cpp
@@ -1,1 +1,21 @@
 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'twoArrays' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY A
+ *  3. INTEGER_ARRAY B
+ */
+
+string twoArrays(int k, vector<int> A, vector<int> B) {
+
+}
+

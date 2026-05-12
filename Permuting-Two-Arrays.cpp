d21e9cb2df3ccce2fd56a314f74e0d6fad25e71f
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-05-12 12:20:34 +0900

    Update Permuting-Two-Arrays.cpp

--- Permuting-Two-Arrays.cpp
+++ Permuting-Two-Arrays.cpp
@@ -16,22 +16,15 @@ vector<string> split(const string &);
  *  3. INTEGER_ARRAY B
  */
 
-#include <vector>
-#include <algorithm>
-#include <string>
-
-string twoArrays(int k, vector<int> A, vector<int> B)
-{
+string twoArrays(int k, vector<int> A, vector<int> B) {
     // Sort A in ascending order
     std::sort(A.begin(), A.end());
 
     // Sort B in descending order
     std::sort(B.begin(), B.end(), std::greater<int>());
 
-    // Pair the smallest element in A
-    // with the largest element in B.
-    // If any pair sum is less than k,
-    // a valid permutation is impossible.
+    // Pair the smallest element in A with the largest element in B.
+    // If any pair sum is less than k, a valid permutation is impossible.
     for (size_t i = 0; i < A.size(); ++i)
     {
         if (A[i] + B[i] < k)
@@ -44,24 +37,6 @@ string twoArrays(int k, vector<int> A, vector<int> B)
     return "YES";
 }
 
-string twoArrays(int k, vector<int> A, vector<int> B)
-{
-    std::string result {"YES"};
-    
-    std::sort(A.begin(), A.end());
-    std::sort(B.begin(), B.end(), greater<int>());
-    
-    for (int i = 0; i < A.size(); ++i)
-    {
-        if (A[i] + B[i] < k)
-        {
-            result = "NO";
-            break;
-        }
-    }
-    
-    return result;
-}
 
 int main()
 {
@@ -118,41 +93,44 @@ int main()
     return 0;
 }
 
-string ltrim(const string &str) {
+string ltrim(const string &str)
+{
     string s(str);
-
     s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+        s.begin(), find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
     );
-
     return s;
 }
 
-string rtrim(const string &str) {
+string rtrim(const string &str)
+{
     string s(str);
-
     s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end()
     );
-
     return s;
 }
 
-vector<string> split(const string &str) {
+std::vector<string> split(const string &str)
+{
     vector<string> tokens;
+    string_view str_view(str);
+    size_t start = 0;
+    size_t end = 0;
 
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
+    while ((end = str_view.find(' ', start)) != string_view::npos)
+    {
+        if (end > start)
+        {
+            tokens.emplace_back(str_view.substr(start, end - start));
+        }
         start = end + 1;
     }
 
-    tokens.push_back(str.substr(start));
+    if (start < str_view.size())
+    {
+        tokens.emplace_back(str_view.substr(start));
+    }
 
     return tokens;
 }

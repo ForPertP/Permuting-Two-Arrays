fe888fee3ed37989ae976f3912b7a27d8f933056
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-05-17 12:44:23 +0900

    Update Permuting-Two-Arrays.py

--- Permuting-Two-Arrays.py
+++ Permuting-Two-Arrays.py
@@ -1,1 +1,17 @@
-Permuting-Two-Arrays
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'twoArrays' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY A
+#  3. INTEGER_ARRAY B
+#

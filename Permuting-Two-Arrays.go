a076c4a2a718726b5896b5d29630296501dd7ff3
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-05-18 14:06:17 +0900

    Update Permuting-Two-Arrays.go

--- Permuting-Two-Arrays.go
+++ Permuting-Two-Arrays.go
@@ -10,3 +10,15 @@ import (
     "strings"
     "sort"
 )
+
+
+
+/*
+ * Complete the 'twoArrays' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY A
+ *  3. INTEGER_ARRAY B
+ */

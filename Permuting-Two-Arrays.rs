0af97504982a07414b09f9d499e962c001b04735
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-05-21 13:38:06 +0900

    Update Permuting-Two-Arrays.rs

--- Permuting-Two-Arrays.rs
+++ Permuting-Two-Arrays.rs
@@ -1,1 +1,15 @@
 Permuting-Two-Arrays
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'twoArrays' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY A
+ *  3. INTEGER_ARRAY B
+ */
